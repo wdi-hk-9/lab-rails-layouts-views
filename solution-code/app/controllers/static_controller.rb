fc5d92521898057dae9ad1ba6ec2b99c7a6ccbff
Denis Cheung <d.s.h.cheung@gmail.com>
2015-10-28 12:21:54 +0800

    updated solution

--- solution-code/app/controllers/static_controller.rb
+++ solution-code/app/controllers/static_controller.rb
@@ -1,10 +1,11 @@
 class StaticController < ApplicationController
   def index
     if params[:page] == "1"
-      render :index_1 and return
+      render :index_1
     elsif params[:page] == "2"
-      render :index_2  and return
+      render :index_2
     end
+    render :index
   end
 
   def page
